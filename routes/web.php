7cdfa52960e03788aa0b72787ee64ad0a744b266
Bayu-x3 <rpsmaster12@gmail.com>
2024-07-26 19:43:30 +0700

    ADD: Routes, Form CRUD

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    FilmController,
+};
+
+Route::get('/', [FilmController::class, 'movieHome'])->name('home');
+Route::get('/movies', [FilmController::class, 'movies'])->name('movies');
+Route::get('/movies/{film}', [FilmController::class, 'show'])->name('movies.show');
+Route::get('/movies/genre/{genre}', [FilmController::class, 'moviesByGenre'])->name('genre');
+Route::resource('film', FilmController::class)->parameters([
+    'film' => 'film'
+]);
